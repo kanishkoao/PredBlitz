c560647c042c2dac9b58d728453a28bd7cf44897
kanishkoao <67572757+kanishkoao@users.noreply.github.com>
2024-07-17 10:29:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PredictionForm from './pred';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>Cricket Prediction App</h1>
+      <PredictionForm />
+    </header>
+  </div>
+);
+
+}
+
+export default App;
